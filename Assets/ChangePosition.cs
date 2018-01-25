ce64bbd82afcf97894c3fd592edd880bba846d20
Tolik <tolik.anishenko@mail.ru>
2018-01-25 13:21:25 +0300

    First Commit
    Added Unity Project to GitHub

---
+++ Assets/ChangePosition.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangePosition : MonoBehaviour {
+	public GameObject curr;
+	public float delt;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = curr.transform.position;
+		if (Input.GetKeyDown (KeyCode.A)) {
+			pos.x += delt;
+		} else if (Input.GetKeyDown (KeyCode.D)) {
+			pos.x -= delt;
+		} else if (Input.GetKeyDown (KeyCode.W)) {
+			pos.y += delt;
+		} else if (Input.GetKeyDown (KeyCode.S)) {
+			pos.y -= delt;
+		} else if (Input.GetKeyDown (KeyCode.Q)) {
+			pos.z += delt;
+		} else if (Input.GetKeyDown (KeyCode.E)) {
+			pos.z -= delt;
+		}
+		curr.transform.position = pos;
+	}
+}
